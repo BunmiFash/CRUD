a22a20bbd1b519d88ad019321103c03c4ace76d6
Bunmi <foluwaboonmy@gmail.com>
2022-06-23 01:50:36 -0700

    'App Created'

---
+++ I4G023599F5X/blog/views.py
@@ -0,0 +1,39 @@
+from django.shortcuts import render
+from django.urls import reverse_lazy
+from .models import Post
+from django.views.generic import ListView
+from django.views.generic.edit import CreateView
+from django.views.generic import DetailView
+from django.views.generic.edit import UpdateView
+from django.views.generic import DeleteView
+
+class PostListView(ListView):
+    model = Post
+    template_name = 'blog/post_list.html'
+
+
+class PostCreateView(CreateView):
+    model = Post    
+    fields = "__all__"
+    success_url = reverse_lazy("blog:all")
+    template_name = 'blog/post_form.html'
+
+
+class PostDetailView(DetailView):
+    model = Post
+    template_name = 'blog/post_detail.html'
+ 
+
+class PostUpdateView(UpdateView):
+    model = Post
+    fields = "__all__"
+    success_url = reverse_lazy("blog:all")
+    template_name = 'base.html'
+
+
+class PostDeleteView(DeleteView):
+    model = Post
+    fields = "__all__"
+    success_url = reverse_lazy("blog:all")
+    template_name = 'blog/post_confirm_delete.html'
+
--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# CRUD
+An App named blog was created.
+The CRUD Method was implemented with Class Based Views.
